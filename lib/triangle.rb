ed9e2dfb37e94714ab60cc82bbda67691fc0cca7
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-02-17 21:54:53 -0500

    commit tree 24543a2e5a65beb84893055e6c25d2f98ec6afab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+
+    if @sides.include?(0) || @sides.any?(&:negative?) || !passes_inequality?
+      puts "Raising! #{@sides}"
+      puts "@sides.include?(0) #{@sides.include?(0)}"
+      puts "@sides.any?(&:negative?) #{@sides.any?(&:negative?)}"
+      puts "!passes_inequality? #{!passes_inequality?}"
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if @sides.uniq.size == 1
+      :equilateral
+    elsif @sides.uniq.size == 2
+      :isosceles
+    elsif @sides.uniq.size == 3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "Problem: @sides"
+  end
+
+  private
+
+  def passes_inequality?
+    sides = @sides.dup
+    largest_side = @sides.max
+    sides.delete_at(sides.index(largest_side))
+
+    if sides.sum > largest_side
+      true
+    else
+      false
+    end
+  end
 end
